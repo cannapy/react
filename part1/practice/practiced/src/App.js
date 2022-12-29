2b923510d4a1c9c39ccdf0fe3e16dc78b5aa8ed8
David Moyer <cannapytents@gmail.com>
2022-12-29 15:56:55 -0500

    Conditional Rendering

--- part1/practice/practiced/src/App.js
+++ part1/practice/practiced/src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState} from 'react'
+const App=()=>{
+  const [left, setLeft]=useState(0)
+  const [right, setRight]=useState(0)
+  const [allClicks,setAll]=useState([])
+  const handleLeftClick=()=>{
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+  }
+  const handleRightClick=()=>{
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+  }
+  return(
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
-  );
+  )
 }
+export default App
+// import {useState} from 'react'
+// const App=()=>{
+//   const [left, setLeft]=useState(0)
+//   const [right, setRight]=useState(0)
+//   const [allClicks,setAll]=useState([])
+//   const handleLeftClick=()=>{
+//     setAll(allClicks.concat('L'))
+//     setLeft(left+1)
+//   }
+//   const handleRightClick=()=>{
+//     setAll(allClicks.concat('R'))
+//     setRight(right+1)
+//   }
+//   return(
+//     <div>
+//       {left}
+//       <button onClick={handleLeftClick}>Left</button>
+//       <button onClick={handleRightClick}>Left</button>
+//       {right}
+//       <p>{allClicks.join(' ')}</p>
+//     </div>
+//   )
+// }
 
-export default App;
+// export default App
+// // import {useState} from 'react'
+// // const App=()=>{
+// //   const [left, setLeft]=useState(0)
+// //   const [right, setRight]=useState(0)
+// //   const [allClicks, setAll]=useState([])
+
+// //   const handleLeftClick=()=>{
+// //     setAll(allClicks.concat('L'))
+// //     setLeft(left+1)
+// //   }
+// //   const handleRightClick=()=>{
+// //     setAll(allClicks.concat('R'))
+// //     setRight(right+1)
+// //   }
+// //   return(
+// //     <div>
+// //       {left}
+// //       <button onClick={handleLeftClick}>Left</button>
+// //       <button onClick={handleRightClick}>Right</button>
+// //       {right}
+// //       <p>{allClicks.join(' ')}</p>
+// //     </div>
+// //   )
+// // }
+// // export default App
+
+
+// // // import {useState} from 'react'
+
+// // // const App=()=>{
+// // //   const [clicks, setClicks]=useState({left:0,right:0})
+// // //   const handleLeftClicks=()=>
+// // //     setClicks({...clicks, left:clicks.left+1})
+// // //   const handleRightClicks=()=>
+// // //     setClicks({...clicks, right:clicks.right+1})
+  
+// // //   return(
+// // //     <div>
+// // //       {clicks.left}
+// // //       <button onClick={handleLeftClicks}>left</button>
+// // //       <button onClick={handleRightClicks}>Right</button>
+// // //       {clicks.right}
+// // //     </div>
+// // //   )
+// // // }
+// // // export default App
+
+
+// // // // import {useState} from 'react'
+// // // // const App = () =>{
+// // // //   const [clicks, setClicks] = useState({
+// // // //     left:0, right:0
+// // // //   })
+  
+// // // //   const handleLeftClicks=()=>
+// // // //     setClicks({...clicks, left: clicks.left+1})
+  
+
+// // // //   const handleRightClicks=()=>
+// // // //     setClicks({...clicks,right:clicks.right+1 })
+
+// // // //   return(
+// // // //     <div>
+// // // //       {clicks.left}
+// // // //       <button onClick={handleLeftClicks}>left</button>
+// // // //       <button onClick={handleRightClicks}>right</button>
+// // // //       {clicks.right}
+// // // //     </div>
+// // // //   )
+// // // // }
+
+// // // // export default App
